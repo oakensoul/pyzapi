6db5c92969e65b0b6e2b2533848f17368ff0f2c0
oakensoul <github@oakensoul.com>
2017-09-06 10:56:33 -0400

    switching to view classes

--- pyzapi/tests_unit.py
+++ pyzapi/tests_unit.py
@@ -11,17 +11,19 @@ class TutorialViewTests(unittest.TestCase):
         testing.tearDown()
 
     def test_home(self):
-        from .views import home
+        from .views import PyzapiViews
 
         request = testing.DummyRequest()
-        response = home(request)
+        inst = PyzapiViews(request)
+        response = inst.home()
 
         self.assertEqual('Home View', response['name'])
 
     def test_hello(self):
-        from .views import hello
+        from .views import PyzapiViews
 
         request = testing.DummyRequest()
-        response = hello(request)
+        inst = PyzapiViews(request)
+        response = inst.hello()
 
         self.assertEqual('Hello View', response['name'])
--- pyzapi/views.py
+++ pyzapi/views.py
@@ -1,13 +1,15 @@
-from pyramid.view import view_config
+from pyramid.view import (view_config, view_defaults)
 
 
-# First view, available at http://localhost:6543/
-@view_config(route_name='home', renderer='home.pt')
-def home(request):
-    return {'name': 'Home View'}
+@view_defaults(renderer='home.pt')
+class PyzapiViews:
+    def __init__(self, request):
+        self.request = request
 
+    @view_config(route_name='home')
+    def home(self):
+        return {'name': 'Home View'}
 
-# /howdy
-@view_config(route_name='hello', renderer='home.pt')
-def hello(request):
-    return {'name': 'Hello View'}
+    @view_config(route_name='hello')
+    def hello(self):
+        return {'name': 'Hello View'}
